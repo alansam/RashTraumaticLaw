ff06526b36093f79e4c74834e56c0b5d6cd559d5
Alan Sampson <alansamps@gmail.com>
2020-09-22 18:18:08 +0000

    Add simple loop samples.

--- main.cpp
+++ main.cpp
@@ -26,7 +26,8 @@ int main(int argc, char const * argv[]) {
   for (std::string tst : tests) {
     bool lc = true;
 
-    std::for_each(tst.begin(), tst.end(), [&lc](char const c_) {
+    std::for_each(tst.begin(), tst.end(),
+      [&lc](char const c_) {
         bool l_ = std::islower(c_);
         lc = lc ? l_ : lc;
 //        std::cout << std::boolalpha << lc << ' ' << l_ << std::endl;
@@ -43,9 +44,10 @@ int main(int argc, char const * argv[]) {
 
   std::cout << "std::find_if()" << std::endl;
   for (std::string tst : tests) {
-    auto fi = std::find_if(tst.begin(), tst.end(), [](char c_) {
-      return !std::islower(c_);
-    });
+    auto fi = std::find_if(tst.begin(), tst.end(),
+      [](char const c_) {
+        return !std::islower(c_);
+      });
     bool lc = (fi != tst.end()) ? false : true;
 
     std::cout << std::setw(20)
@@ -59,9 +61,10 @@ int main(int argc, char const * argv[]) {
 
   std::cout << "std::find_if_not()" << std::endl;
   for (std::string tst : tests) {
-    auto fi = std::find_if_not(tst.begin(), tst.end(), [](char c_) {
-      return std::islower(c_);
-    });
+    auto fi = std::find_if_not(tst.begin(), tst.end(),
+      [](char const c_) {
+        return std::islower(c_);
+      });
     bool lc = (fi != tst.end()) ? false : true;
 
     std::cout << std::setw(20)
@@ -75,9 +78,44 @@ int main(int argc, char const * argv[]) {
 
   std::cout << "std::all_of()" << std::endl;
   for (std::string tst : tests) {
-    auto lc = std::all_of(tst.begin(), tst.end(), [](char c_) {
-      return std::islower(c_);
-    });
+    auto lc = std::all_of(tst.begin(), tst.end(),
+      [](char const c_) {
+        return std::islower(c_);
+      });
+
+    std::cout << std::setw(20)
+              << tst
+              << ": "
+              << std::boolalpha
+              << lc
+              << std::endl;
+  }
+  std::putchar('\n');
+
+  std::cout << "loopy" << std::endl;
+  for (std::string tst : tests) {
+    bool lc = true;
+    for (auto ch : tst) {
+      if(!(lc = std::islower(ch))) { break; }
+    }
+
+    std::cout << std::setw(20)
+              << tst
+              << ": "
+              << std::boolalpha
+              << lc
+              << std::endl;
+  }
+  std::putchar('\n');
+
+
+  std::cout << "classic loop" << std::endl;
+  for (std::string tst : tests) {
+    bool lc = true;
+    for (size_t c_ = 0; c_ < tst.length(); ++c_) {
+      lc = std::islower(tst[c_]);
+      if (!lc) { break; }
+    }
 
     std::cout << std::setw(20)
               << tst
