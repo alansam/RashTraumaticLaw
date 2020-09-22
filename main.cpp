9ccd70ec45852fbbf0e025fe35ccbac0877b3c2c
Alan Sampson <alansamps@gmail.com>
2020-09-22 21:40:32 +0000

    Add a lambda only sample.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ int main(int argc, char const * argv[]) {
     "whatever next",
   };
 
+  //  for_each()
   std::cout << "std::for_each()" << std::endl;
   for (std::string tst : tests) {
     bool lc = true;
@@ -42,6 +43,7 @@ int main(int argc, char const * argv[]) {
   }
   std::putchar('\n');
 
+  //  find_if()
   std::cout << "std::find_if()" << std::endl;
   for (std::string tst : tests) {
     auto fi = std::find_if(tst.begin(), tst.end(),
@@ -59,6 +61,7 @@ int main(int argc, char const * argv[]) {
   }
   std::putchar('\n');
 
+  //  find_if_not()
   std::cout << "std::find_if_not()" << std::endl;
   for (std::string tst : tests) {
     auto fi = std::find_if_not(tst.begin(), tst.end(),
@@ -76,6 +79,7 @@ int main(int argc, char const * argv[]) {
   }
   std::putchar('\n');
 
+  //  all_of()
   std::cout << "std::all_of()" << std::endl;
   for (std::string tst : tests) {
     auto lc = std::all_of(tst.begin(), tst.end(),
@@ -92,6 +96,7 @@ int main(int argc, char const * argv[]) {
   }
   std::putchar('\n');
 
+  //  loop over
   std::cout << "loopy" << std::endl;
   for (std::string tst : tests) {
     bool lc = true;
@@ -108,7 +113,7 @@ int main(int argc, char const * argv[]) {
   }
   std::putchar('\n');
 
-
+  //  classic loop
   std::cout << "classic loop" << std::endl;
   for (std::string tst : tests) {
     bool lc = true;
@@ -124,6 +129,25 @@ int main(int argc, char const * argv[]) {
               << lc
               << std::endl;
   }
+  std::putchar('\n');
+
+  //  do it all with lambdas
+  std::cout << "do it all with lambdas" << std::endl;
+  std::for_each(tests.begin(), tests.end(), 
+    [](auto tst) {
+      auto fi = std::find_if(tst.begin(), tst.end(),
+        [](auto c_) {
+          return !std::islower(c_);
+        });
+      auto lc = (fi != tst.end()) ? false : true;
+
+      std::cout << std::setw(20)
+                << tst
+                << ": "
+                << std::boolalpha
+                << lc
+                << std::endl;
+    });
   std::putchar('\n');
 
   return 0;
